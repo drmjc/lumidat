b822a02cd8e07e1187e93e6fd4eae9278fa7b4f4
Mark Cowley <m.cowley0@gmail.com>
2012-10-14 11:49:16 +0000

    added 2 parameters: zipfile, clmfile; bug fix: properly send the probeID to the java code bug fix: when prefix is specified, the correct filenames are returned

--- R/preprocess.illumina.idat.R
+++ R/preprocess.illumina.idat.R
@@ -8,6 +8,13 @@
 #' sample and ProbeID naming, however we cannot guarantee that the output will be identical to
 #' that produced by GenomeStudio.
 #'
+#' @section Reading iDAT files:
+#' iDAT files can be specified in 3 main ways, either as a character vector of file paths, via
+#' the \code{files} parameter; (2) a character vector of filenames, and the path to the directy
+#' containing the files via \code{files} and \code{path} arguments; (3) via a single zip file
+#' containing iDAT files, via the \code{zipfile} argument. The zip file may contain subfolders,
+#' though each array should have a unique name, regardless of where it sits in the zipfile structure.
+#' 
 #' @section Array manifest files:
 #' Array manifest files can be downloaded from Illumina [2,3]. It is important to
 #'  use the TXT version, not the BGX version. You can use a newer release of the manifest file 
@@ -34,7 +41,7 @@
 #' Control Probe Profile.txt files}
 #' \item{\dQuote{max}}{Choose the probe with the highest AVG_Intensity value}
 #' \item{\dQuote{median}}{Choose the probe with the median AVG_Intensity value}
-#' \item{\dQuote{mean}}{Determing the mean fluorescence across all probes}
+#' \item{\dQuote{mean}}{Determine the mean fluorescence across all probes}
 #' }
 #' Choosing either \dQuote{max}, \dQuote{median} or \dQuote{mean} creates 
 #' \dQuote{Sample Gene Profile.txt} and \dQuote{Control Gene Profile.txt} files. 
@@ -70,7 +77,13 @@
 #' @param files A character vector of at least one file name
 #' @param path The path to the directories where the files are. This defaults to the current
 #'  working directory.
-#' @param manifestfile The full path to the Array manifest file in TXT format.
+#' @param zipfile the path to a zip file containing idat files. This over-rides the \code{files} argument.
+#' @param manifestfile The full path to the Array manifest file in TXT format. See \code{\link{list_illumina_manifest_files}}
+#' and then \code{\link{download_illumina_manifest_file}} to download manifest files directly from Illumina.
+#' @param clmfile \code{NULL}, or the path to a GenePattern-CLM file [5]. This represents a mechanism
+#' for renaming, and reordering the samples in the resulting object. Column 1 is the idat name (not path),
+#' Column 2 is the desired sample name, column 3 is the biological class of each sample. The order of the
+#' rows specifies the order of the samples in the result.
 #' @param probeID This controls which value to identify each probe by. Allowable values are 
 #'    \dQuote{ArrayAddressID}, \dQuote{ProbeID}, \dQuote{Sequence}, \dQuote{NuID}.
 #' @param collapseMode Collapse probes to genes using the specified mode. Valid values are 
@@ -81,7 +94,7 @@
 #' @param outdir The full path to the output directory. If \code{NULL} the current working directory is used.
 #' @param prefix An character[1] used as a file name prefix for the files that will be created.
 #'    \dQuote{NONE} (the default) means no prefix will be used.
-#' @param verbose logical, if TRUE, print informative messages
+#' @param verbose logical, if \code{TRUE}, print informative messages
 #' @param memory The maximum Java memory heapsize. Default -Xmx1024m, which allows 1GB of RAM.
 #' @return If collapseMode=\dQuote{NONE}, then invisbly return a character[2] containing the 
 #'   file paths of the \dQuote{Sample Probe Profile.txt} and \dQuote{Control Probe Profile.txt}
@@ -96,25 +109,55 @@
 #' [3] \url{http://www.switchtoi.com/annotationprevfiles.ilmn} \cr
 #' [4] Du, P., Kibbe, W. A., & Lin, S. M. (2007). nuID: a universal naming scheme of oligonucleotides
 #' for illumina, affymetrix, and other microarrays Biol Direct, 2, 16. (doi:10.1186/1745-6150-2-16) \cr
+#' [5] \url{http://www.broadinstitute.org/cancer/software/genepattern/tutorial/gp_fileformats.html#CLM}
 #' @export
 #' @examples
+#' # iDAT files+path as input
 #' path <- system.file("extdata", package="lumidat")
 #' outdir <- tempdir()
 #' files <- c("5356583020_A_Grn.idat", "5356583020_B_Grn.idat")
 #' manifestfile <- system.file("extdata", "HumanHT-12_V3_0_R1_99999999.txt", package="lumidat")
-#' files <- preprocess.illumina.idat(files, path, manifestfile, probeID="NuID", outdir=outdir)
+#' files <- preprocess.illumina.idat(files, path, manifestfile=manifestfile, probeID="NuID", outdir=outdir)
 #' files
 #'
-preprocess.illumina.idat <- function(files=NULL, path=NULL, manifestfile=NULL, 
-	probeID=c("ArrayAddressID", "ProbeID", "Sequence", "NuID"), 
+#' # zipfile as input
+#' path <- system.file("extdata", package="lumidat")
+#' files <- c("5356583020_A_Grn.idat", "5356583020_B_Grn.idat")
+#' zipfile <- tempfile(fileext=".zip")
+#' zip(zipfile, file.path(path, files))
+#' files <- preprocess.illumina.idat(zipfile=zipfile, manifestfile=manifestfile, probeID="NuID", outdir=outdir)
+#' files
+#' 
+#' # CLM file
+#' path <- system.file("extdata", package="lumidat")
+#' outdir <- tempdir()
+#' files <- c("5356583020_A_Grn.idat", "5356583020_B_Grn.idat")
+#' manifestfile <- system.file("extdata", "HumanHT-12_V3_0_R1_99999999.txt", package="lumidat")
+#' clmfile <- system.file("extdata", "5356583020.clm", package="lumidat")
+#' files <- preprocess.illumina.idat(files, path, manifestfile=manifestfile, clmfile=clmfile, probeID="NuID", outdir=outdir)
+#' readLines(files[2], 2)
+#' # [1] "TargetID\tProbeID\tA.AVG_Signal\tA.BEAD_STDERR\tA.Avg_NBEADS\tA.Detection Pval\tB.AVG_Signal\tB.BEAD_STDERR\tB.Avg_NBEADS\tB.Detection Pval"
+#' # [2] "biotin\t0gwIEN5441dbo3j27A\t16902.39\t5062.077\t85\t1.0000000\t16668.27\t6532.465\t60\t1.0000000"
+#' 
+preprocess.illumina.idat <- function(files=NULL, path=NULL, zipfile=NULL, manifestfile=NULL, clmfile=NULL,
+	probeID=c("ProbeID", "ArrayAddressID", "Sequence", "NuID"), 
 	collapseMode=c("none", "max", "median", "mean"), 
 	backgroundCorrect=FALSE,
-	outdir=NULL, prefix=NULL, verbose=TRUE, memory="-Xmx1024m") {
+	outdir=NULL, prefix=NULL, verbose=FALSE, memory="-Xmx1024m") {
 	####################
 	# check function arguments
 	#
 	
 	# Check files + path
+	if( !is.null(zipfile) ) {
+		files <- NULL
+		path <- NULL
+		dir <- tempdir()
+		# OSX Finder adds ._ files.
+		files <- unzip(zipfile, files=grep("MACOSX", as.character(unzip(zipfile, list=TRUE)$Name), invert=TRUE, value=TRUE), exdir=dir, junkpaths=TRUE, overwrite=FALSE)
+		on.exit(unlink(files))
+	}
+	
 	files <- unique(files)
 	length(files) > 0 || stop("Must supply at least 1 input file.")
 	
@@ -125,7 +168,7 @@ preprocess.illumina.idat <- function(files=NULL, path=NULL, manifestfile=NULL,
 	else {
 		files <- file.path(path, files)
 		if( !all(file.exists(files)) )
-			stop("files+path must specify a vector of valid file names.")
+			stop("path+files must specify a vector of valid file names.")
 	}
 	
 	# check the manifestfile
@@ -134,11 +177,8 @@ preprocess.illumina.idat <- function(files=NULL, path=NULL, manifestfile=NULL,
 	if( tolower(substring(manifestfile, nchar(manifestfile)-3, nchar(manifestfile))) == "bgx" )
 		stop("You must use a TXT manifest file, not the BGX version.")
 	
-	# check probeID parameter.
-	probeID <- match.arg(probeID[1], c("ArrayAddressID", "ProbeID", "Sequence", "NuID"), several.ok=FALSE)
-
-	# check probeID parameter.
-	collapseMode <- match.arg(collapseMode[1], c("none", "max", "median", "mean"), several.ok=FALSE)
+	collapseMode <- match.arg(collapseMode)
+	probeID <- match.arg(probeID)
 	
 	# check outdir
 	if( is.null(outdir) ) outdir <- "." 
@@ -152,6 +192,10 @@ preprocess.illumina.idat <- function(files=NULL, path=NULL, manifestfile=NULL,
 	
 	# memory
 	substring(memory, 1, 4) == "-Xmx" || stop("Invalid memory parameter. eg -Xmx2048m for 2GB RAM")
+	
+	# clmfile
+	clmfileFlag <- ""
+	if( !is.null(clmfile) ) clmfileFlag <- paste("-clmfile", shQuote(clmfile))
 	####################
 	
 	####################
@@ -162,34 +206,35 @@ preprocess.illumina.idat <- function(files=NULL, path=NULL, manifestfile=NULL,
 	file.exists(jar) || stop("Can't find jar.")
 	
 	# setup the command line
-	quiet <- ifelse(verbose, "", "-quiet")
-	flags <- sprintf("-outputDir %s -bg %s -collapse %s -manifestfile %s %s %s", 
+	# quietFlag <- ifelse(verbose, "", "-quiet")
+	quietFlag <- ""
+	flags <- sprintf("-outputDir %s -bg %s -collapse %s -manifestfile %s -probeID %s %s %s %s", 
 		outdir, 
 		backgroundCorrect,
 		collapseMode,
 		manifestfile,
+		probeID,
 		prefixFlag,
-		quiet
+		clmfileFlag,
+		quietFlag
 	)
 	files2 <- paste(shQuote(files), collapse=" ")
 	cmd <- paste("java", memory, "-jar", jar, flags, files2)
-	if(verbose) cat(cmd, "\n")
+	# if(verbose) cat(cmd, "\n")
 	####################
 	
 	####################
 	# run the Java command
-	system(cmd, intern=FALSE)
+	lumidat.log <- system(cmd, intern=TRUE)
+	if( length(grep("^Wrote:", lumidat.log)) != 2 ) {
+		cat("Couldn't find the 2 output files. here's the run-time log:\n")
+		cat(lumidat.log)
+	}
+	else if( verbose ) {
+		cat(lumidat.log)
+	}
+	res <- file.path(outdir, sub("Wrote: ", "", grep("Wrote:", lumidat.log, value=TRUE)))
 	####################
 
-	####################
-	# If it worked, then these files should have been created:
-	filename.spp <- file.path(outdir, "Sample Probe Profile.txt")
-	file.exists(filename.spp) || stop("Can't find the Sample Probe Profile.txt file created by running GenomeStudioFileCreator-1.0.jar")
-	
-	filename.cpp <- file.path(outdir, "Control Probe Profile.txt")
-	file.exists(filename.cpp) || stop("Can't find the Control Probe Profile.txt file created by running GenomeStudioFileCreator-1.0.jar")
-	####################
-
-	res  <- c(filename.spp, filename.cpp)
 	invisible(res)
 }
